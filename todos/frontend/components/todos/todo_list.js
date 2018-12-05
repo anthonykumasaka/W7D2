2ff51439142f54f39d8b8757891ea85848c0746e
anthonykumasaka <anthonykumasaka@gmail.com>
2018-12-04 18:44:21 -0800

    Part 2

--- todos/frontend/components/todos/todo_list.js
+++
@@ -1,2 +0,0 @@
-
-module.exports = () => <h3>Say Hello to my lil List</h3>;
--- todos/frontend/reducers/selectors.js
+++
@@ -1,8 +0,0 @@
-export const allTodos = state => {
-  let todoKeys = Object.keys(state.todos)
-  return todoKeys.reduce((acc, key) => {
-    return [...acc, state.todos[key]]; 
-  }, 
-  [])
-};
-
